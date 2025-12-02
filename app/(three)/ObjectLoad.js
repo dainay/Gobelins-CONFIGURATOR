0126cc0c1fb095ecaedd4ed9222b657d8dd47fcd
dainay <115573080+dainay@users.noreply.github.com>
2025-12-02 10:11:06 +0100

    lqst chqnges

--- app/(three)/ObjectLoad.js
+++ app/(three)/ObjectLoad.js
@@ -2,37 +2,48 @@ import { useEffect } from "react";
 import { Asset } from "expo-asset";
 import { useGLTF, useTexture } from "@react-three/drei/native";
 import * as THREE from "three";
+import Model from "../../assets/models/skin.glb"
 
 export default function ObjectLoad() {
-  // 1. Загружаем свою текстуру
-  const cheeseTexture = useTexture(
-    require("../../assets/images/texture.jpeg")
+ 
+  const gltf = useGLTF(Model);
+  return (
+    <mesh>
+      <primitive position={[0, 0, 0]} object={gltf.scene} scale={1} />
+    </mesh>
   );
-
-  // 2. Загружаем GLB из URL
-  const asset = Asset.fromURI(
-    "https://t49q8flueooukrdm.public.blob.vercel-storage.com/gobelinsV3.glb"
-  );
-
-  const { nodes, materials, scene } = useGLTF(asset.uri, true);
-
-
-  // 3. После загрузки — заменить материалы
-  useEffect(() => {
-    if (!scene) return;
-
-    console.log(nodes, materials);
-
-    scene.traverse((obj) => {
-      if (obj.isMesh) {
-        obj.material = new THREE.MeshStandardMaterial({
-          map: cheeseTexture,
-        });
-        obj.castShadow = true;
-        obj.receiveShadow = true;
-      }
-    });
-  }, [scene, cheeseTexture]);
-
-  return <primitive object={scene} scale={0.5} />;
 }
+
+
+  // // 1. Загружаем свою текстуру
+  // const cheeseTexture = useTexture(
+  //   require("../../assets/images/texture.jpeg")
+  // );
+
+  // // 2. Загружаем GLB из URL
+  // const asset = Asset.fromURI(
+  //   "https://t49q8flueooukrdm.public.blob.vercel-storage.com/gobelinsV3.glb"
+  // );
+
+  // const { nodes, materials, scene } = useGLTF(asset.uri, true);
+
+
+  // // 3. После загрузки — заменить материалы
+  // useEffect(() => {
+  //   if (!scene) return;
+
+  //   console.log(nodes, materials);
+
+  //   scene.traverse((obj) => {
+  //     if (obj.isMesh) {
+  //       obj.material = new THREE.MeshStandardMaterial({
+  //         map: cheeseTexture,
+  //       });
+  //       obj.castShadow = true;
+  //       obj.receiveShadow = true;
+  //     }
+  //   });
+  // }, [scene, cheeseTexture]);
+
+  // return <primitive object={scene} scale={0.5} />;
+// }
